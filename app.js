0ae63733d24dc5cac40fb669d9902f61d618da91
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-10 14:11:21 +0530

    29 basics: Printing request url, method and headers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log('url', req.url);
+  console.log('method', req.method);
+  console.log('headers', req.headers);
+  // process.exit();
 });
 
 server.listen(3000);
