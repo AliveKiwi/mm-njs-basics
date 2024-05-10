018e03d6b00ef5edea87b298bb31745b4fda67e7
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-10 15:02:10 +0530

    32 basics: Added a route on path /

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('url', req.url);
-  console.log('method', req.method);
-  console.log('headers', req.headers);
-  // process.exit();
+  const { url } = req;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text name="message"/><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end(); // If we don't put it then the request will go on to next set of instructions
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('/<html>');
+  res.write('</html>');
   res.end();
 });
 
