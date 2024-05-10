611809a65bdc0bf57d90b23cc319fa28bf6926d0
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-11 01:01:25 +0530

    73 express: Added a helper/util method to generate root Directory using path.dirname(require.main.filename)

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // We are using get method to send the form, and action in form /add-product call post middleware
 router.get('/add-product', (req, res, next) => {
   // The path must be an absolute path
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
