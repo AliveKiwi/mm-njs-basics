bdde1da74c9ef7c1e64955c0cb699a6b898071c0
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-10 17:34:17 +0530

    60 express: Demonstrating the use of next to go on to next middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+// next is passed to use method to allow express to move on to next middleware
+app.use((req, res, next) => {
+  console.log('I am the middlewre');
+  next(); // If we don't call it then the request won't go to next app.use()
+});
+
+app.use((req, res, next) => {
+  console.log('I am another middlewre');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
