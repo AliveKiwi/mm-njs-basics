3ce24c8aebc7484cb7a1cd03635679d9ac0c3443
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-10 14:17:55 +0530

    30 basics: Sending a basic response from server using response setHeader, write and end

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const server = http.createServer((req, res) => {
   console.log('method', req.method);
   console.log('headers', req.headers);
   // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('/<html>');
+  res.end();
 });
 
 server.listen(3000);
