0c462ac6a3bc68d480ad886c1a81ebb76fb38b9f
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-11 17:49:25 +0530

    86 pug: Added dynamic active class using path configuration in main-layout.pug

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-  res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: '404' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,10 @@ router.get('/add-product', (req, res, next) => {
   // The path must be an absolute path
   // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { pageTitle: 'Add Product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
